d457e6990e966f8b9e5651981de8bc26a3ba4196
Angel-H-ernandez <104273138+Angel-H-ernandez@users.noreply.github.com>
2026-06-05 21:31:28 -0600

    Initial Commit

---
+++ RegistroTDC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RegistroTDC
+//
+//  Created by angel hernandez on 05/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RegistroTDC/RegistroTDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistroTDCApp.swift
+//  RegistroTDC
+//
+//  Created by angel hernandez on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RegistroTDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
